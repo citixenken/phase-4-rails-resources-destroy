09c1c1eb736a0dd4021eca2d862edce0a36fa1ba
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-28 10:47:30 +0300

    COMPLETED: Rails Resource Routing-Destroy

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -44,6 +44,19 @@ class BirdsController < ApplicationController
     end
   end
 
+  # DELETE /birds/:id
+  def destroy 
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy # remove from DB
+      head :no_content #HTTP code 204: server has successfully fulfilled the request and that there is no content to send in the response
+      
+      # render json: {} # returns empty object
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
